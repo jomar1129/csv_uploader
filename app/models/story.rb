eb0bbd567bd46004adcd8a221abe6155b0eabc29
jomar1129 <jomarcotejo@gmail.com>
2021-11-07 16:09:15 +1100

    fix error , and do migration for db

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,9 +1,8 @@
 class Story < ApplicationRecord
     def self.import(file)
         converter = lambda { |header| header.downcase }
-        ::CSV.foreach(file.path, headers: true , skip_blanks: true , encoding:'iso-8859-1:utf-8' , header_converters: converter).reject do |row|
-            puts row[0]
-            # Story.create! (row.to_hash)  
+        ::CSV.foreach(file.path, headers: true , encoding:'iso-8859-1:utf-8', header_converters: converter) do |row|
+            Story.create! (row.to_hash)
         end
     end
 end
