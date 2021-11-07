748866b6dc51b939eb49ad18e370ac00b6dbd12c
jomar1129 <jomarcotejo@gmail.com>
2021-11-07 16:14:47 +1100

    redo index controller to use pagination and remove unsused controllers

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -3,60 +3,11 @@ class StoriesController < ApplicationController
 
   # GET /stories or /stories.json
   def index
-    @stories = Story.all
+   @stories = Story.order(params[:sort]).page(params[:page]).per(10)
   end
 
-  # GET /stories/1 or /stories/1.json
-  def show
-  end
 
-  # GET /stories/new
-  def new
-    @story = Story.new
-  end
-
-  # GET /stories/1/edit
-  def edit
-  end
-
-  # POST /stories or /stories.json
-  def create
-    @story = Story.new(story_params)
-
-    respond_to do |format|
-      if @story.save
-        format.html { redirect_to @story, notice: "Story was successfully created." }
-        format.json { render :show, status: :created, location: @story }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @story.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /stories/1 or /stories/1.json
-  def update
-    respond_to do |format|
-      if @story.update(story_params)
-        format.html { redirect_to @story, notice: "Story was successfully updated." }
-        format.json { render :show, status: :ok, location: @story }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @story.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /stories/1 or /stories/1.json
-  def destroy
-    @story.destroy
-    respond_to do |format|
-      format.html { redirect_to stories_url, notice: "Story was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
-    # to import CSV
+  # to import CSV
 
   def import
     Story.import(params[:file])
