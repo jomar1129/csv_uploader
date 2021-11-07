bd25fcbc993a8b7baae9a90f966df43fb12f1c62
jomar1129 <jomarcotejo@gmail.com>
2021-11-07 16:37:18 +1100

    added search for name on the table?

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -3,7 +3,14 @@ class StoriesController < ApplicationController
 
   # GET /stories or /stories.json
   def index
-   @stories = Story.order(params[:sort]).page(params[:page]).per(10)
+
+    # if search is found call the function
+    if params[:search]
+      search_table
+    end
+
+    # if non is found will force to show all current data?
+    @stories = Story.order(params[:sort]).page(params[:page]).per(10)
   end
 
 
@@ -13,6 +20,15 @@ class StoriesController < ApplicationController
     Story.import(params[:file])
     redirect_to stories_path, notice: "Stories Added Successfully!"
   end
+
+  #search function
+
+  def search_table
+    if @stories = Story.all.find{ |story| story.name.downcase.include?(params[:search].downcase)}
+      redirect_to stories_path(@stories)
+    end
+  end
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
