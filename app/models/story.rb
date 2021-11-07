aa851e3e42ccb46fd5c387bde76486e69417a133
jomar1129 <jomarcotejo@gmail.com>
2021-11-07 16:03:44 +1100

    edit story controller and added csv importer

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,2 +1,9 @@
 class Story < ApplicationRecord
+    def self.import(file)
+        converter = lambda { |header| header.downcase }
+        ::CSV.foreach(file.path, headers: true , skip_blanks: true , encoding:'iso-8859-1:utf-8' , header_converters: converter).reject do |row|
+            puts row[0]
+            # Story.create! (row.to_hash)  
+        end
+    end
 end
